52df3282b3aee8dfbeb257537afa1e85615e025d
Wanessa Dias <wanessadias@ioasys.com.br>
2024-08-13 11:14:41 -0300

    aula intro

--- Introdução/Estacionamento.java
+++ Introdução/Estacionamento.java
@@ -1,39 +1,23 @@
-/*# Ocultamento de Informações
-Suponha que a classe abaixo seja utilizada em um sistema de estacionamentos. Esta classe não oculta estruturas que podem mudar no futuro. Reescreva este código, visando encapsular a estrutura de dados responsável por armazenar veículos, isto é, a tabela hash. 
-
 import java.util.Hashtable;
-
-public class Estacionamento {
-
-  public Hashtable<String, String> veiculos;
-
-  public Estacionamento() {
-    veiculos = new Hashtable<String, String>();
-  }
-
-  public static void main(String[] args) {
-    Estacionamento e = new Estacionamento();
-    e.veiculos.put("TCP-7030", "Uno");
-    e.veiculos.put("BNF-4501", "Gol");
-    e.veiculos.put("JKL-3481", "Corsa");
-  }
-}
-
-*/
-
-import java.util.Hashtable;
+import java.util.NoSuchElementException;
 
 public class Estacionamento {
 
   private Hashtable<String, String> veiculos;
+  private Gerente gerente;
 
   public void estacionar(String placa, String modelo) {
-    // regras de negocio
+    if(placa == null || placa.length() < 8){
+      throw new NoSuchElementException("Placa inválida");
+    }
+    else if(modelo==null){
+      throw new NoSuchElementException("Modelo inválido");
+    }
     this.veiculos.put(placa, modelo);
   }
 
-  public Estacionamento() {
+  public Estacionamento(Gerente gerente) {
     veiculos = new Hashtable<String, String>();
-
+    this.gerente = gerente;
   }
 }
--- Introdução/Gerente.java
+++ Introdução/Gerente.java
@@ -1,46 +1,46 @@
 public class Gerente{
-    private String nome;
-    private String cpf;
-    private String fone;
-    private String endereco;
-  
-    Gerente(String nome, String cpf, String fone, String endereco){
-        this.nome = nome;
-        this.cpf = cpf;
-        this.fone = fone;
-        this.endereco = endereco;
-      
-    }
-  
-    public String getNome(){
-      return this.nome;
-    }
-  
-    public String getCpf(){
-      return this.cpf;
-    }
-  
-    public String getFone(){
-      return this.fone;
-    }
-  
-    public String getEndereco(){
-      return endereco;
-    }
-  
-    public void setNome(String nome){
+  private String nome;
+  private String cpf;
+  private String fone;
+  private String endereco;
+
+  Gerente(String nome, String cpf, String fone, String endereco){
       this.nome = nome;
-    }
-  
-    public void setCpf(String cpf){
       this.cpf = cpf;
-    }
-  
-    public void setFone(String fone){
       this.fone = fone;
-    }
-  
-    public void setEndereco(String endereco){
       this.endereco = endereco;
-    }
+    
   }
+
+  public String getNome(){
+    return this.nome;
+  }
+
+  public String getCpf(){
+    return this.cpf;
+  }
+
+  public String getFone(){
+    return this.fone;
+  }
+
+  public String getEndereco(){
+    return endereco;
+  }
+
+  public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public void setCpf(String cpf){
+    this.cpf = cpf;
+  }
+
+  public void setFone(String fone){
+    this.fone = fone;
+  }
+
+  public void setEndereco(String endereco){
+    this.endereco = endereco;
+  }
+}
